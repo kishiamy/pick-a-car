4f3f073c974f1789406a448c6902021fdb06cf36
kishiamy <b.feblespadron@gmail.com>
2020-07-21 14:30:42 +0200

    add makes page

--- components/Card.vue
+++ components/Card.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="card">
-    <div class="card-image" />
+  <div class="card round shadow">
+    <!-- <div class="card-image"> -->
+    <img src="~/assets/car1.jpg" class="card-image" alt="cardImage" />
+    <!-- </div> -->
     <div class="card-content">{{ content }}</div>
   </div>
 </template>
@@ -14,42 +16,59 @@ export default Vue.extend({
       type: String,
       default: true,
     },
+    image: {
+      type: String,
+    },
   },
 })
 </script>
 
 <style lang="scss" scoped>
 .card {
-  //   display: -ms-flexbox;
-  //   display: -webkit-flex;
-  //   display: flex;
-  //   -webkit-flex-direction: column;
-  //   -ms-flex-direction: column;
-  //   flex-direction: column;
-  //   display: flex;
-  //   -webkit-flex-wrap: nowrap;
-  //   -ms-flex-wrap: nowrap;
-  //   flex-wrap: nowrap;
-  //   -webkit-justify-content: space-around;
-  //   -ms-flex-pack: space-around;
-  //   justify-content: space-around;
-  //   -webkit-align-content: space-around;
-  //   -ms-flex-line-pack: space-around;
-  //   align-content: space-around;
-  //   -webkit-align-items: stretch;
-  //   -ms-flex-align: stretch;
-  //   align-items: stretch;
-  border: 1px solid black;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-flex-direction: column;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  display: flex;
+  -webkit-flex-wrap: nowrap;
+  -ms-flex-wrap: nowrap;
+  flex-wrap: nowrap;
+  -webkit-justify-content: space-around;
+  -ms-flex-pack: space-around;
+  justify-content: space-around;
+  -webkit-align-content: space-around;
+  -ms-flex-line-pack: space-around;
+  align-content: space-around;
+  -webkit-align-items: stretch;
+  -ms-flex-align: stretch;
+  align-items: stretch;
+
   margin: 1rem 1rem;
   width: 312px;
 }
 .card-image {
-  background-image: url('~assets/car1.jpg');
+  //   background-image: url('~assets/car1.jpg');
   background-size: cover;
   width: 312px;
   height: 156px;
+  //   display: -ms-flexbox;
+  //   display: -webkit-flex;
+  //   display: flex;
 }
 .card-content {
   padding: 1rem 1rem;
+}
+.shadow {
+  -moz-box-shadow: 5px 5px 5px rgba(68, 68, 68, 0.6);
+  -webkit-box-shadow: 5px 5px 5px rgba(68, 68, 68, 0.6);
+  box-shadow: 5px 5px 5px rgba(68, 68, 68, 0.6);
+}
+.round {
+  -webkit-border-radius: 8px;
+  -moz-border-radius: 8px;
+  border-radius: 8px;
+  overflow: hidden;
 }
 </style>
--- pages/makes.vue
+++ pages/makes.vue
@@ -1,19 +1,12 @@
 <template>
   <div>
-    <div>
-      <nuxt-link to="/">
-        <h1>
-          Pick a Car
-        </h1>
-      </nuxt-link>
-    </div>
-
-    <nuxt-link to="makes" class="flex-container"> See Makes</nuxt-link>
+    <Make class="flex-container" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import Make from 'components/Make.vue'
 
 export default Vue.extend({})
 </script>
@@ -39,19 +32,4 @@ export default Vue.extend({})
   -ms-flex-align: center;
   align-items: center;
 }
-
-// .flex-item {
-//   display: -ms-flexbox;
-//   display: -webkit-flex;
-//   display: flex;
-//   -webkit-order: 0;
-//   -ms-flex-order: 0;
-//   order: 0;
-//   -webkit-flex: 0 1 auto;
-//   -ms-flex: 0 1 auto;
-//   flex: 0 1 auto;
-//   -webkit-align-self: center;
-//   -ms-flex-item-align: center;
-//   align-self: center;
-// }
 </style>
